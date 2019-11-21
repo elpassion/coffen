975ff9a277be69c6801763a1c8541587d3aa564f
Mariusz <insanepl@gmail.com>
2019-11-21 12:47:22 +0100

    Redux auth example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./stores/rootReducer";
+import { authSlice } from "./stores/auth";
 
 function App() {
+  const authState = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
   return (
     <div>
       <header>
-        <p>
+        <p onClick={() => dispatch(authSlice.actions.authenticate())}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
--- src/stores/auth.tsx
+++ src/stores/auth.tsx
@@ -1,31 +1,9 @@
-// Actions
-export const SET_USER_AUTHENTICATED = "auth.SET_USER_AUTHENTICATED";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Reducer
-const initialState = {
-  isAuthenticated: false
-};
-
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case SET_USER_AUTHENTICATED:
-      return {
-        ...state,
-        isAuthenticated: true
-      };
-    default:
-      return state;
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: false,
+  reducers: {
+    authenticate: () => true
   }
-}
-
-// Action Creators
-export function userLogIn() {
-  return { type: SET_USER_AUTHENTICATED };
-}
-
-// Side Effects
-export function setAuthenticated() {
-  return dispatch => {
-    dispatch(userLogIn());
-  };
-}
+});
