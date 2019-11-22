217969db5e9f37582fb399b5d525e445156fa23a
Mariusz <insanepl@gmail.com>
2019-11-22 14:51:32 +0100

    Counter sections

--- src/containers/Brew/style.ts
+++ src/containers/Brew/style.ts
@@ -6,5 +6,12 @@ import { size, font } from "styles";
 export const StepsWrapper = styled.ul`
   margin: 0;
   padding: 0;
+  list-style: none;
+  counter-reset: section;
+`;
+
+export const BrewStep = styled.li`
   background: red;
+  counter-increment: section;
+  content: "Section " counter(section) ": ";
 `;
