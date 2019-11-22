5628c38828a10e36dc353b78321513ddf693e9f9
Michał Warda <michal.warda@elpassion.pl>
2019-11-22 10:33:12 +0100

    Extract brewratingform

--- src/containers/Brew/index.tsx
+++ src/containers/Brew/index.tsx
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
-import { BrewBasicsForm, BrewingBasicsFormValues } from "./components/BrewBasicsForm";
-import { BrewCustomizationForm, BrewingCustomizationFormValues } from "./components/BrewCustomizationForm";
-import { SubmitButton } from "components/Form/SubmitButton";
-import { Form } from "react-final-form";
-import { ErrorsForValues } from "components/Form/types";
-import { SelectField, SelectFieldOption } from "components/Form/SelectField";
 import { useHistory } from "react-router";
 import { Routing } from "utils/routing";
-
-interface BrewRatingFormValues {
-  rating: number;
-  comment: string;
-}
+import { BrewBasicsForm, BrewingBasicsFormValues } from "./components/BrewBasicsForm";
+import { BrewCustomizationForm, BrewingCustomizationFormValues } from "./components/BrewCustomizationForm";
+import { BrewRatingForm } from "./components/BrewRatingForm";
 
 export const Brew = () => {
   const [brewBasics, setBrewBasics] = useState<BrewingBasicsFormValues | undefined>(undefined);
@@ -20,39 +12,14 @@ export const Brew = () => {
   );
   const history = useHistory();
 
-  const saveBrew = (brewRatingValues: BrewRatingFormValues) => {
-    history.push(Routing.Feed);
-  };
-
   return (
     <>
       <h1>New brew</h1>
       {brewCustomizationData && brewBasics ? (
-        <Form
-          onSubmit={saveBrew}
-          subscription={{}}
-          validate={values => {
-            const errors: ErrorsForValues<BrewRatingFormValues> = {};
-            if (!values.rating) errors.rating = "Required";
-            return errors;
-          }}
-          render={({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <p>Coffee Name: {brewBasics.coffeeName}</p>
-              <p>Technique: {brewBasics.technique}</p>
-              <p>Water: {brewCustomizationData.waterDose}</p>
-              <p>Coffee Weight: {brewCustomizationData.coffeeWeight}</p>
-              <p>Grind Size: {brewCustomizationData.grindSize}</p>
-              <SelectField name="rating" label="Rating">
-                {Array.from({ length: 10 }, (_, index) => (
-                  <SelectFieldOption value={index} key={index}>
-                    ${index}
-                  </SelectFieldOption>
-                ))}
-              </SelectField>
-              <SubmitButton title="Save brew"></SubmitButton>
-            </form>
-          )}
+        <BrewRatingForm
+          onSubmit={brewRatingValues => history.push(Routing.Feed)}
+          brewBasics={brewBasics}
+          brewCustomizationData={brewCustomizationData}
         />
       ) : brewBasics ? (
         <BrewCustomizationForm onSubmit={brewCustomizationData => setBrewCustomizationData(brewCustomizationData)} />
