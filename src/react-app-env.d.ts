bbefc0812ed5d0c36cf314f588ab4ec14761b5ba
Michał Warda <michal.warda@elpassion.pl>
2019-12-06 15:11:42 +0100

    Extract screenshot to paparazzi

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module "jsdom-screenshot" {
-  function generateImage(options: any): Promise<void>;
-}
--- src/test/utils/index.tsx
+++ src/test/utils/index.tsx
@@ -1,18 +1,4 @@
-import isCi from "is-ci";
-import { generateImage } from "jsdom-screenshot";
-import open from "open";
-import tmp from "tmp";
-
-export const screenshot = async () => {
-  const path = tmp.fileSync({ postfix: ".png" }).name;
-  await generateImage({
-    screenshot: { path },
-    launch: {
-      defaultViewport: { width: 375, height: 812 }
-    }
-  });
-  await open(path, { wait: false });
-};
+import { takeScreenshotOnTestError } from "@error-paparazzi/jest";
 
 export const supressActWarnings = () => {
   const originalError = console.error;
@@ -30,29 +16,7 @@ export const supressActWarnings = () => {
   });
 };
 
-export const takeScreenshotOnError = () => {
-  const makeScreenshotOnFail = (fn: any) => async () => {
-    try {
-      await fn();
-    } catch (e) {
-      if (!isCi) await screenshot();
-      throw e;
-    }
-  };
-
-  const wrapApply = (target: jest.It, that: any, args: Parameters<jest.It>) => {
-    args[1] = makeScreenshotOnFail(args[1]);
-    target.apply(that, args);
-  };
-
-  test = new Proxy(test, { apply: wrapApply });
-  test.only = new Proxy(test.only, { apply: wrapApply });
-  test.each = new Proxy(test.each, {
-    apply: (target, that, args) => new Proxy(target.apply(that, args), { apply: wrapApply })
-  });
-};
-
 export const setupBrowserTests = () => {
   supressActWarnings();
-  takeScreenshotOnError();
+  takeScreenshotOnTestError({ launchOptions: { defaultViewport: { width: 375, height: 812 } } });
 };
